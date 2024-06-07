bbb571eaa06afa232a5c5d64268d64875a52be93
Babak <babakhanibabak@gmail.com>
2024-06-07 11:42:17 +0330

    Complete LogInScreen

--- app/src/main/java/com/example/calculator/ui/logInScreen/LogInScreen.kt
+++ app/src/main/java/com/example/calculator/ui/logInScreen/LogInScreen.kt
@@ -8,10 +8,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.calculator.ui.theme.CalculatorTheme
@@ -29,14 +32,20 @@ fun LogInScreen(
     ) {
         Button(
             onClick = { /*TODO*/ },
-            modifier = Modifier.padding(32.dp).fillMaxWidth()
+            modifier = Modifier
+                .padding(32.dp)
+                .fillMaxWidth(),
+            colors =ButtonDefaults.buttonColors(Color.Green)
         ) {
             Text(text = "BasicCalculatorScreen")
         }
         Spacer(modifier = Modifier.size(50.dp))
         Button(
             onClick = { /*TODO*/ },
-            modifier = Modifier.padding(32.dp).fillMaxWidth()
+            modifier = Modifier
+                .padding(32.dp)
+                .fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(Color.Magenta)
         ) {
             Text(text = "CalculatorScreen")
         }
