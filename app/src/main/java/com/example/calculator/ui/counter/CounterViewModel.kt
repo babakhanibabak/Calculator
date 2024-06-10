4e20cec74c1932e47986c67d6b33dfff5ab33d19
Babak Babakhani <=>
2024-06-10 18:50:48 +0100

    Added coin list image

--- app/src/main/java/com/example/calculator/ui/counter/CounterViewModel.kt
+++ app/src/main/java/com/example/calculator/ui/counter/CounterViewModel.kt
@@ -1,16 +1,31 @@
 package com.example.calculator.ui.counter
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CounterViewModel @Inject constructor() : ViewModel() {
 
     private val _uiState = MutableStateFlow(0)
-    val uiState = _uiState.asStateFlow()
+    val uiState by lazy {
+        countToTen()
+        _uiState.asStateFlow()
+    }
+
+    private fun countToTen() {
+        viewModelScope.launch {
+            for (i in 1..10) {
+                delay(1000)
+                _uiState.value = i
+            }
+        }
+    }
 
     fun onPlusClick() {
         _uiState.value++
--- app/src/main/java/com/example/calculator/ui/cryptolist/CryptoListScreen.kt
+++ app/src/main/java/com/example/calculator/ui/cryptolist/CryptoListScreen.kt
@@ -1,5 +1,6 @@
 package com.example.calculator.ui.cryptolist
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -19,6 +21,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -92,6 +95,11 @@ fun CoinItem(
             .clickable { onClick(model) }
     ) {
         Row(modifier = Modifier.fillMaxWidth()) {
+            Image(
+                modifier = Modifier.size(64.dp),
+                painter = painterResource(id = model.photo),
+                contentDescription = "",
+            )
             Text(text = model.name)
         }
     }
