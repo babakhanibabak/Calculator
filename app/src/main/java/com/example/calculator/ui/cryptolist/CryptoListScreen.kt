9ec245b63013c5a08b2175d7b945d8d0813d3808
Babak <babakhanibabak@gmail.com>
2024-06-12 18:07:09 +0330

    Add Appbar for CryptoListScreen

--- app/src/main/java/com/example/calculator/ui/cryptolist/CryptoListScreen.kt
+++ app/src/main/java/com/example/calculator/ui/cryptolist/CryptoListScreen.kt
@@ -9,12 +9,18 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -22,8 +28,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.calculator.ui.theme.CalculatorTheme
 
@@ -66,16 +74,40 @@ private fun CryptoListScreenContent(
     }
 }
 
+
+
 @Composable
 fun CoinsList(dataList: List<CoinListUiModel>) {
-    LazyColumn(
-        contentPadding = PaddingValues(start = 16.dp, top = 64.dp, end = 16.dp, bottom = 16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        items(dataList) {
-            CoinItem(model = it)
+
+    Scaffold(topBar = {
+        AddAppBar()
+    },
+        content = { padding ->
+            Surface(modifier = Modifier.padding(padding)) {
+                LazyColumn(
+                    contentPadding = PaddingValues(
+                        start = 16.dp,
+                        top = 64.dp,
+                        end = 16.dp,
+                        bottom = 16.dp
+                    ),
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    items(dataList) {
+                        CoinItem(model = it)
+                    }
+                }
+            }
         }
-    }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddAppBar(modifier: Modifier = Modifier) {
+    TopAppBar(modifier = modifier,
+        colors = TopAppBarDefaults.topAppBarColors(Color.Magenta),
+        title = { Text(text = "Crypto List", textAlign = TextAlign.Center, fontSize = 32.sp) })
 }
 
 @Composable
@@ -104,9 +136,9 @@ fun CoinItem(
                 painter = painterResource(id = model.photo),
                 contentDescription = "",
             )
-            Text(text = model.name,modifier=Modifier.align(Alignment.CenterVertically))
-            Text(text = model.id,modifier=Modifier.align(Alignment.CenterVertically))
-            Text(text = model.symbol,modifier=Modifier.align(Alignment.CenterVertically))
+            Text(text = model.name, modifier = Modifier.align(Alignment.CenterVertically))
+            Text(text = model.id, modifier = Modifier.align(Alignment.CenterVertically))
+            Text(text = model.symbol, modifier = Modifier.align(Alignment.CenterVertically))
 
         }
     }
