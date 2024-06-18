33064cdb9b87f898e529f0a2d72cc77d2a1e8955
Babak Babakhani <=>
2024-06-18 17:35:09 +0100

    MyAppBar added

---
+++ app/src/main/java/com/example/calculator/ui/components/MyAppBar.kt
@@ -0,0 +1,45 @@
+package com.example.calculator.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyAppBar(
+    modifier: Modifier = Modifier,
+    title: String = "",
+    onBackClick: () -> Unit = {},
+) {
+    TopAppBar(
+        modifier = modifier,
+        colors = TopAppBarDefaults.topAppBarColors(Color.LightGray),
+        title = {
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = title,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Medium,
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = onBackClick) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = null,
+                )
+            }
+        }
+    )
+}
