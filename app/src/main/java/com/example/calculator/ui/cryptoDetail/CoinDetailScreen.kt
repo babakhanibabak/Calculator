f447b7f70c5b55df978f618106b9cf3224a76526
Babak <babakhanibabak@gmail.com>
2024-06-22 09:58:32 +0330

    Design for detail screen

--- app/src/main/java/com/example/calculator/ui/cryptoDetail/CoinDetailScreen.kt
+++ app/src/main/java/com/example/calculator/ui/cryptoDetail/CoinDetailScreen.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -33,10 +35,11 @@ fun CoinDetailScreen(
     viewModel: CoinDetailViewModel = hiltViewModel(),
     onBackClick: () -> Unit = {},
 
-) {
+    ) {
     val uiState by viewModel.uiState.collectAsState()
 
-    CoinDetailScreenContent(uiState = uiState, onBackClick = onBackClick
+    CoinDetailScreenContent(
+        uiState = uiState, onBackClick = onBackClick
     )
 }
 
@@ -50,9 +53,11 @@ private fun CoinDetailScreenContent(
         modifier = modifier.fillMaxSize(),
         topBar = { MyAppBar(title = "Coin Detail") { onBackClick() } },
     ) { paddingValues ->
-        Box(modifier = modifier
-            .fillMaxSize()
-            .padding(paddingValues)) {
+        Box(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
             when {
                 uiState.isLoading -> {
                     CircularProgressIndicator(
@@ -76,53 +81,77 @@ fun CoinDetail(
     uiState: CoinDetailScreenState,
 ) {
     Column(
-        modifier = modifier.fillMaxSize().padding(top = 15.dp),
+        modifier = modifier
+            .fillMaxSize()
+            .padding(top = 15.dp),
         verticalArrangement = Arrangement.spacedBy(20.dp)
     ) {
         uiState.showDetailData?.let { data ->
             // TODO: Design
-             Row(modifier=Modifier.align(Alignment.CenterHorizontally)) {
-                 data.photo?.let {
-                     CircleImage(modifier = Modifier,
-                         imageId = it)
-                 }
-             }
+            Row(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                data.photo?.let {
+                    CircleImage(
+                        modifier = Modifier,
+                        imageId = it
+                    )
+                }
+            }
             Spacer(modifier = Modifier.size(10.dp))
-           Row (
-               modifier=Modifier.fillMaxWidth()
-           ){
-               Text(
-                   text = "Detail",
-                   fontSize = 20.sp,
-                   color = Color.LightGray
-               )
-               Spacer(modifier = Modifier.size(25.dp))
-               Text(
-                   modifier = Modifier.fillMaxWidth(),
-                   text = data.message.orEmpty(),
-                   fontSize = 20.sp,
-                   color = Color.Blue,
-                   textAlign = TextAlign.Left
-               )
-           }
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Detail",
+                    fontSize = 20.sp,
+                    color = Color.LightGray
+                )
+                Spacer(modifier = Modifier.size(25.dp))
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = data.message.orEmpty(),
+                    fontSize = 20.sp,
+                    color = Color.Blue,
+                    textAlign = TextAlign.Left
+                )
+            }
+            HorizontalDivider(color = Color.LightGray)
             Spacer(modifier = Modifier.size(10.dp))
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Text(
+                    text = "Description",
+                    fontSize = 20.sp,
+                    color = Color.LightGray
+                )
+                Spacer(modifier = Modifier.size(25.dp))
+                Text(
+                    text = data.description.orEmpty(),
+                    modifier = Modifier.fillMaxWidth(),
+                    fontSize = 15.sp,
+                    color = Color.Blue
+                )
+            }
+            HorizontalDivider(color = Color.LightGray, thickness = 3.dp)
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Text(text = "First Price",
+                    fontSize = 20.sp,
+                    color = Color.Gray)
+                Spacer(modifier = Modifier.size(25.dp))
+                Text(
+                    text = data.firstPrice.toString(),
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
 
-            Text(
-                text = data.description.orEmpty(),
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            )
-            Spacer(modifier = Modifier.size(10.dp))
-
-            Text(
-                text = data.firstPrice.toString(),
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            )
-
-            Spacer(modifier = Modifier.size(10.dp))
-            Text(
-                text = data.lastPrice.toString(),
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            )
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Text(text = "Last Price" ,
+                    fontSize = 20.sp,
+                    color = Color.Gray)
+                Spacer(modifier = Modifier.size(25.dp))
+                Text(
+                    text = data.lastPrice.toString(),
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
         }
     }
 }
@@ -136,7 +165,9 @@ private fun CoinDetailScreenPreview() {
             uiState = CoinDetailScreenState(
                 showDetailData = CoinDetailUiModel(
                     message = "Detail",
-                    description = "Description",
+                    description = "Bitcoin is the world's first decentralized cryptocurrency." +
+                            " A cryptocurrency is a digital asset that uses cryptography" +
+                            " for security and operates independently of a central bank or authority.",
                     firstPrice = 20,
                     lastPrice = 50,
                     photo = R.drawable.bit
