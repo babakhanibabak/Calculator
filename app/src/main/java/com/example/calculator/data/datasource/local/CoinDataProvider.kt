48646b53bcb7dcce5c8ec1db0a30cae21f71a699
Babak <babakhanibabak@gmail.com>
2024-06-22 09:57:32 +0330

    Add description text

--- app/src/main/java/com/example/calculator/data/datasource/local/CoinDataProvider.kt
+++ app/src/main/java/com/example/calculator/data/datasource/local/CoinDataProvider.kt
@@ -21,7 +21,7 @@ class CoinDataProvider @Inject constructor() {
             description = coin?.description.orEmpty(),
             firstPrice = coin?.firstPrice ?: 0,
             lastPrice = coin?.lastPrice ?: 0,
-            photo = coin?.photo ?:0
+            photo = coin?.photo ?: 0
 
         )
     }
@@ -34,38 +34,51 @@ class CoinDataProvider @Inject constructor() {
             rank = 1,
             photo = R.drawable.bit,
             message = "Bitcoin Message",
-            description = "Bitcoin Description",
-            firstPrice = 10,
+            description = "Bitcoin is the world's first decentralized cryptocurrency." +
+                    " A cryptocurrency is a digital asset that uses cryptography" +
+                    " for security and operates independently of a central bank or authority.",
+            firstPrice = 520,
             lastPrice = 64000,
         ),
         CoinListItemModel(
             id = "2",
             name = "Tether",
-            symbol = "tet",
+            symbol = "TET",
             rank = 2,
             photo = R.drawable.tether,
             message = "Tether Message",
-            description = "Tether Description",
-            firstPrice = 10,
+            description = "Tether, often abbreviated as USDT or USD₮ " +
+                    "is a type of cryptocurrency known as a stablecoin." +
+                    " Unlike traditional cryptocurrencies like Bitcoin," +
+                    " which can experience significant price swings, "
+            ,
+            firstPrice = 410,
             lastPrice = 3000,
         ),
         CoinListItemModel(
             id = "3",
             name = "Ethereum",
-            symbol = "eth",
+            symbol = "ETH",
             rank = 3,
             photo = R.drawable.ethereum,
             message = "Ethereum Message",
-            description = "Ethereum Description"
+            description = "Ethereum is a revolutionary blockchain platform " +
+                    "  that goes beyond just being a cryptocurrency.",
+            firstPrice = 250,
+            lastPrice = 2400
         ),
         CoinListItemModel(
             id = "4",
-            name = "USDC",
-            symbol = "usdc",
+            name = "usdc",
+            symbol = "USDC",
             rank = 4,
             photo = R.drawable.usdc,
             message = "USDT Message",
-            description = "USDT Description"
+            description = "USD Coin (USDC) is another type of cryptocurrency known as a stablecoin." +
+                    " Similar to Tether (USDT), USDC aims to maintain a stable value by being pegged " +
+                    "to the US dollar (USD).Ideally, 1 USDC should always be worth close to \$1 USD. ",
+            firstPrice = 310,
+            lastPrice = 4200
         ),
     )
 }
