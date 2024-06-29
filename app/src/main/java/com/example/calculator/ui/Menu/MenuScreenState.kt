7b7781384ee11d3e3e8e6c9117b0092aeb7b8979
Babak <babakhanibabak@gmail.com>
2024-06-29 13:00:06 +0330

    Add State of menu Screen

---
+++ app/src/main/java/com/example/calculator/ui/Menu/MenuScreenState.kt
@@ -0,0 +1,8 @@
+package com.example.calculator.ui.menu
+
+data class MenuScreenState(
+    val favorite:Boolean=false,
+    val date:Boolean=false,
+    val place:Boolean=false,
+    val email:Boolean=false
+)
--- app/src/main/java/com/example/calculator/ui/Menu/MenuScreenViewModel.kt
+++ app/src/main/java/com/example/calculator/ui/Menu/MenuScreenViewModel.kt
@@ -1,15 +1,27 @@
 package com.example.calculator.ui.menu
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MenuScreenViewModel @Inject constructor() : ViewModel() {
-    private val _uiState = MutableStateFlow("")
-    val uiState = _uiState.asStateFlow()
+    private val _uiState = MutableStateFlow(MenuScreenState())
 
+    val uiState by lazy {
+        onFavoriteClick()
+        _uiState.asStateFlow()
+    }
+
+    private fun onFavoriteClick() {
+        viewModelScope.launch {
+            _uiState.update { it.copy(favorite = true) }
+        }
+    }
 
 }
