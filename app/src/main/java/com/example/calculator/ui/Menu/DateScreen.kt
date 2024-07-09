b5428022125b14a0cfc26f10d9c072b952d0c628
Babak <babakhanibabak@gmail.com>
2024-07-09 20:09:23 +0330

    complete DateScreen

--- app/src/main/java/com/example/calculator/ui/Menu/DateScreen.kt
+++ app/src/main/java/com/example/calculator/ui/Menu/DateScreen.kt
@@ -1,22 +1,59 @@
 package com.example.calculator.ui.menu
 
+import android.icu.util.Calendar
+import android.icu.util.TimeZone
+import android.os.Build
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SelectableDates
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.calculator.ui.theme.CalculatorTheme
+import java.time.DayOfWeek
+import java.time.Instant
+import java.time.ZoneId
 
 
 @Composable
-fun DateScreen(modifier: Modifier = Modifier) {
+fun DateScreen() {
     DateScreenContent()
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DateScreenContent(modifier: Modifier = Modifier) {
-    val datePickerState= rememberDatePickerState()
+    val datePickerState = rememberDatePickerState(
+        initialDisplayedMonthMillis = System.currentTimeMillis(),
+        yearRange = 1900..2024,
+        selectableDates = object : SelectableDates {
+            override fun isSelectableDate(utcTimeMillis: Long): Boolean {
+                return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    val dayOfWeek = Instant.ofEpochMilli(utcTimeMillis).atZone(ZoneId.of("UTC"))
+                        .toLocalDate().dayOfWeek
+                    dayOfWeek != DayOfWeek.SUNDAY && dayOfWeek != DayOfWeek.SATURDAY
+                } else {
+                    val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+                    calendar.timeInMillis = utcTimeMillis
+                    calendar[Calendar.DAY_OF_WEEK] != Calendar.SUNDAY &&
+                            calendar[Calendar.DAY_OF_WEEK] != Calendar.SATURDAY
+                }
+            }
+
+            override fun isSelectableYear(year: Int): Boolean {
+                return true
+            }
+        }
+    )
+    DatePickerDialog(onDismissRequest = { /*TODO*/ },
+        confirmButton = { /*TODO*/ },
+        dismissButton = {},
+        content = { DatePicker(state = datePickerState)}
+    )
+        
+
 }
 
 @Preview
