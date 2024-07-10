cac842f904aa31e46182538c58ec257e1470d43b
Babak <babakhanibabak@gmail.com>
2024-07-10 23:32:26 +0330

    Create MapScreen

---
+++ app/src/main/java/com/example/calculator/ui/Menu/MapScreen.kt
@@ -0,0 +1,9 @@
+package com.example.calculator.ui.menu
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MapScreen(modifier: Modifier = Modifier) {
+
+}
