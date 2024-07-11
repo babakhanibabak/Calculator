d47948fec41df220d5041b01d7becc66084e0b3c
Babak <babakhanibabak@gmail.com>
2024-07-11 13:15:18 +0330

    Complete GoogleMapScreen

--- app/src/main/java/com/example/calculator/ui/Menu/GoogleMapScreen.kt
+++ app/src/main/java/com/example/calculator/ui/Menu/GoogleMapScreen.kt
@@ -1,6 +1,6 @@
 package com.example.calculator.ui.menu
 
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -9,6 +9,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.calculator.ui.theme.CalculatorTheme
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
@@ -25,26 +26,23 @@ fun MapScreen() {
 }
 
 @Composable
-fun MapScreenContent(
-    modifier: Modifier = Modifier,
-
-    ) {
+fun MapScreenContent() {
     val atasehir = LatLng(41.015137, 28.979530)
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(atasehir, 15f)
     }
     var uiSettings by remember { mutableStateOf(MapUiSettings(zoomControlsEnabled = true)) }
     var properties by remember { mutableStateOf(MapProperties(mapType = MapType.SATELLITE)) }
-    GoogleMap(
-        modifier = modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState,
-        uiSettings = uiSettings,
-        properties = properties
-    ) {
-        Marker(
-            state = MarkerState(position = atasehir),
-            title = "One Marker"
-        )
+    Box {
+        GoogleMap(
+            modifier = Modifier.matchParentSize(),
+            cameraPositionState = cameraPositionState,
+            uiSettings = uiSettings,
+            properties = properties
+        ) {
+            Marker(icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE),
+                state = MarkerState(position = atasehir),)
+        }
     }
 }
 
