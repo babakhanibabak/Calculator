59dd2aae1b563ae6db211dcc71a696c135f9791f
Babak Babakhani <=>
2024-07-14 20:03:50 +0100

    Renamed menu package

--- app/src/main/java/com/example/calculator/ui/menu/googleMap/GoogleMapScreen.kt
+++ app/src/main/java/com/example/calculator/ui/menu/googleMap/GoogleMapScreen.kt
@@ -2,16 +2,14 @@ package com.example.calculator.ui.menu.googleMap
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.calculator.ui.theme.CalculatorTheme
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
@@ -26,21 +24,23 @@ fun MapScreen() {
 fun MapScreenContent(
     viewModel: MapScreenViewModel = hiltViewModel()
 ) {
-    val atasehir = LatLng(41.015137, 28.979530)
+    val uiState by viewModel.uiState.collectAsState()
+
     val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(atasehir, 15f)
+        position = CameraPosition.fromLatLngZoom(uiState.currentLocation, 15f)
     }
-    var uiSettings by remember { viewModel.uiSettings }
-    var properties by remember { viewModel.properties }
+
     Box {
         GoogleMap(
             modifier = Modifier.matchParentSize(),
             cameraPositionState = cameraPositionState,
-            uiSettings = uiSettings,
-            properties = properties
+            uiSettings = uiState.uiSettings,
+            properties = uiState.mapProperties,
         ) {
-            Marker(icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE),
-                state = MarkerState(position = atasehir))
+            Marker(
+                icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE),
+                state = MarkerState(position = uiState.currentLocation)
+            )
         }
     }
 }
---
+++ app/src/main/java/com/example/calculator/ui/menu/googleMap/MapScreenViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.calculator.ui.menu.googleMap
+
+import androidx.lifecycle.ViewModel
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapUiSettings
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class MapScreenViewModel @Inject constructor() : ViewModel() {
+
+    private val _uiState = MutableStateFlow(
+        MapScreenState(
+            uiSettings = MapUiSettings(),
+            mapProperties = MapProperties(),
+            currentLocation = LatLng(41.015137, 28.979530),
+        )
+    )
+    val uiState by lazy { _uiState.asStateFlow() }
+
+    fun changeLocation() {
+        _uiState.update {
+            it.copy(currentLocation = LatLng(43.015137, 40.979530))
+        }
+    }
+}
