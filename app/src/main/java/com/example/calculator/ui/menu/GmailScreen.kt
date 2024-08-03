66005763f129bf58620199ebc5584eb18530263d
Babak <babakhanibabak@gmail.com>
2024-08-03 22:28:22 +0330

    Create GmailScreen

---
+++ app/src/main/java/com/example/calculator/ui/menu/GmailScreen.kt
@@ -0,0 +1,38 @@
+package com.example.calculator.ui.menu
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.calculator.ui.theme.CalculatorTheme
+
+@Composable
+fun GmailScreen() {
+    GmailScreenContent()
+}
+
+@Composable
+fun GmailScreenContent(modifier: Modifier = Modifier) {
+    Scaffold(modifier = modifier.fillMaxSize(),
+
+        ) { padding ->
+        Column(modifier = Modifier.padding(padding)) {
+
+        }
+
+    }
+}
+
+
+@Preview
+@Composable
+private fun GmailScreenPreview() {
+    CalculatorTheme {
+        GmailScreenContent(
+
+        )
+    }
+}
